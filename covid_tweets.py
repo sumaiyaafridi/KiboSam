c71045fdce3b4b77fc56d254791726504c368abc
sumaiyaafridi <sumaiya.afridi@gmail.com>
2022-10-19 22:20:18 +0500

    Create covid_tweets.py

---
+++ covid_tweets.py
@@ -0,0 +1,14 @@
+
+
+def cleanTweets(tweets):
+  # TODO: implement this function
+    import pandas as pd
+    Import re
+
+    df = pd.read_excel ('tweets')  
+
+
+    return user, usertweets;
+
+Excel = 'Tweets_Dataset.xlsx'
+cleanTweets(tweetsexcel)
